ac894d8677a86dbe2372e6db99e39c511b615079
Antonios Sotiriou <antonios.sotiriou@hotmail.gr>
2022-05-16 00:06:19 +0300

    Testing: Multiprocessing.Small progress but not yet functioning.

--- iterator.c
+++ iterator.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <X11/Xlocale.h>
 
 // signals
 #include <signal.h>
@@ -11,31 +12,31 @@
 #include <semaphore.h>
 
 #include "header_files/objects.h"
-#include "header_files/palette.h"
+#include "header_files/semaphores.h"
+#include "header_files/sem_var.h"
 
 int iterator(Object obj, int pid) {
 
+    sem_init(&sem, 1, sem_var);
+
     Object *shmem;
     key_t key = 9999;
-    int shmid = shmget(key, sizeof(Object) * sizeof(obj), 0666 | IPC_CREAT);
+    int shmid = shmget(key, sizeof(Object), 0666 | IPC_CREAT);
     shmem = shmat(shmid, NULL, 0);
 
-    obj.image_data = calloc(1, sizeof(char) * obj.winattr->width * obj.winattr->height * 4);
-    if (obj.image_data == NULL) {
-        printf("Not Enough memory for the image_data to be allocated...Exit status 1\n");
-        free(obj.image_data);
-        exit(1);
-    }
-
     *shmem = obj;
- 
+    shmctl(shmid, IPC_RMID, 0);
+
     printf("Iterator, preparing to send signal\n");
 
     // thats the signal for the child process to write in the shared memory...
     kill(pid, SIGUSR1); ////////////////////////////////////////////////////
     // here probably i must wait for the child process to write the data before paint the pixmap.
+    // sem_wait(&sem);
     printf("Iterator, signal has been sended\n");
 
-    free(obj.image_data);
+    sem_close(&sem);
+    shmdt(shmem);
+
     return 0;
 }
