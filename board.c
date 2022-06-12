7e7a535f4e6cff870683bbe18ce4805973d8b04a
Antonios Sotiriou <antonios.sotiriou@hotmail.gr>
2022-06-12 13:55:20 +0300

    A working without errors exapmle.Problems only on Map, Unmap for second time.Half window is getting drawed.

--- board.c
+++ board.c
@@ -4,7 +4,7 @@
 #include <errno.h>
 
 // multiprocessing includes
-// #include <unistd.h>
+#include <unistd.h>
 // #include <sys/wait.h>
 #include <pthread.h>
 
@@ -21,24 +21,44 @@
 #include "header_files/locale.h"
 #include "header_files/objects.h"
 
-int painter();
-int receiver();
-void *oscillator();
-int transmitter();
-int clientmessage(XEvent *event);
-int visibilitychange(XEvent *event);
-int mapnotify(XEvent *event);
-int unmapnotify(XEvent *event);
-int expose(XEvent *event);
-int buttonpress(XEvent *event);
-int board();
-
 #define EXIT_FAILURE_NEGATIVE -1
 #define POINTERMASKS ( ButtonPressMask )
 #define KEYBOARDMASKS ( KeyPressMask )
 #define EXPOSEMASKS ( StructureNotifyMask | SubstructureNotifyMask | VisibilityChangeMask | ExposureMask )
 
 enum { App_Close, App_Name, App_Iconify, Atom_Type, Atom_Last};
+
+typedef struct {
+    
+    unsigned int width;
+    unsigned int height;
+    int iterations;
+    int counter;
+    int x;
+    int y;
+    double horiz; 
+    double vert;
+    double zoom; 
+    double init_x; 
+    double init_y;
+    int step_counter;
+    int step_x;
+    int step_y;
+
+} Mandelbrot;
+
+static void mandelbrot_init(Mandelbrot *md);
+void painter(const Mandelbrot md, char *image_data);
+void receiver(Mandelbrot md);
+void *oscillator(void *args);
+const int transmitter(void);
+const void clientmessage(XEvent *event);
+void visibilitychange(XEvent *event);
+const void mapnotify(XEvent *event);
+void unmapnotify(XEvent *event);
+void expose(XEvent *event);
+void buttonpress(XEvent *event);
+int board();
 
 // Global Variables
 Display *displ;
@@ -47,14 +67,14 @@ XSetWindowAttributes set_app;
 Window app;
 static int screen;
 static int RUNNING = 1; 
-static const int THREADS_NUM = 7;                      // Number of Processes.Value can vary from 1 to 10 Excluding(3, 6, 7, 9).More than 10 is overkill.
+static const int THREADS_NUM = 7;                      // The number of Threads.
 static const int WIDTH = 800;                          // App starting window width.
 static const int HEIGHT = 800;                         // App starting window height.
-// static const int ITERATIONS = 1000;                    // Number of mandelbrot iterations.
-// static const double HORIZONTAL = 2.00;                 // Horizontal scale.
-// static const double VERTICAL = 2.00;                   // Vetical scale.
-// static const double ZOOM = 4.00;                       // Mandelbrot starting zoom.
-static int (*handler[LASTEvent]) (XEvent *event) = {
+static const int ITERATIONS = 1000;                    // Number of mandelbrot iterations.
+static const double HORIZONTAL = 2.00;                 // Horizontal scale.
+static const double VERTICAL = 2.00;                   // Vetical scale.
+static const double ZOOM = 4.00;                       // Mandelbrot starting zoom.
+static void (*handler[LASTEvent]) (XEvent *event) = {
 	[ClientMessage] = clientmessage,
 	// [ConfigureRequest] = configurerequest,
 	// [ConfigureNotify] = configurenotify,
@@ -74,45 +94,103 @@ static int (*handler[LASTEvent]) (XEvent *event) = {
 static Atom wmatom[Atom_Last];
 
 /* ##################################################################################################################### */
-int painter() {
-    
+static void mandelbrot_init(Mandelbrot *md) {
+    md->width = stat_app.width;
+    md->height = stat_app.height;
+    md->iterations = ITERATIONS;
+    md->horiz = HORIZONTAL; 
+    md->vert = VERTICAL;
+    md->zoom = ZOOM;
+    md->init_x = 0.00;
+    md->init_y = 0.00;
+}
+/* ##################################################################################################################### */
+void painter(const Mandelbrot md, char *image_data) {
+
+    double a = (md.x - (md.width / md.horiz)) / (md.width / md.zoom) + md.init_x;
+    double b = (md.y - (md.height / md.vert)) / (md.height / md.zoom) + md.init_y;
+    double curr_a = a;
+    double curr_b = b;
+
+    int n = 0;
+    while (n < md.iterations) {
+        double iter_a = (a * a) - (b * b);
+        double iter_b = 2 * a * b;
+        a = iter_a + curr_a;
+        b = iter_b + curr_b;
+
+        if (abs(a + b) > 4) {
+            break;
+        }
+        n++;
+    }
+    if (n < md.iterations && n >= 255)
+        n = n / 100;
+    if (n < md.iterations && n < 255) {
+        image_data[md.counter] =  n + 10;
+        image_data[md.counter + 1] = n + n;
+        image_data[md.counter + 2] =  n * 20;
+    } else {
+        if (image_data[md.counter] != 0x00 || image_data[md.counter + 1] != 0x00 || image_data[md.counter + 2] != 0x00) {
+            image_data[md.counter] =  0x00;
+            image_data[md.counter + 1] = 0x00;
+            image_data[md.counter + 2] = 0x00;
+        }
+    }
+}
+/* ##################################################################################################################### */
+void receiver(Mandelbrot md) {
+
+    XImage *image;
+    char image_data[((md.width * md.height) / THREADS_NUM) * 4];
+
     /* Add grafical context */
     XGCValues gc_values;
     gc_values.foreground = 0xffffff;
-    GC rectangle = XCreateGC(displ, app, GCForeground, &gc_values);
+    GC graphics = XCreateGC(displ, app, GCForeground, &gc_values);
 
-    // printf("Execution reached painter ##########################\n");
-    XDrawRectangle(displ, app, rectangle, rand() % 400, rand() % 400, rand() % 200, rand() % 200);
-    XFreeGC(displ, rectangle);
+    int counter = md.step_counter;
+    int x = md.step_x;
+    int y = md.step_y;
 
-    return EXIT_SUCCESS;
-}
-/* ##################################################################################################################### */
-int receiver() {
+    for (int i = 0; i < (md.width * md.height) / THREADS_NUM; i++) {
 
-    // printf("Execution reached receiver ##########################\n");
-    return EXIT_SUCCESS;
+        if (x == md.width) {
+            y += 1;
+            x = 0;
+        }
+
+        md.x = x;
+        md.y = y;
+        md.counter = counter;
+        painter(md, image_data);
+        counter += 4;
+        x++;
+    }
+
+    image = XCreateImage(displ, stat_app.visual, stat_app.depth, ZPixmap, 0, image_data, md.width, md.height / THREADS_NUM, 32, 0);
+    XPutImage(displ, app, graphics, image, 0, 0, 0, md.step_y, md.width, md.height / THREADS_NUM);
+    XFreeGC(displ, graphics);
+    XFree(image);
 }
 /* ##################################################################################################################### */
 void *oscillator(void *args) {
 
-    int WAITING = 1;
-    int id = *(int*)args;
+    Mandelbrot md;
+    mandelbrot_init(&md);
 
-    printf("Execution reached oscillator ########################## args: %d\n", id);
+    int thread_id = *(int*)args;
+    
+    md.step_counter = 0;
+    md.step_y = (md.height / THREADS_NUM) * thread_id;
 
-    if (WAITING) {
-        printf("LOOPING\n");
-        painter();
-        WAITING = 0;
-    }
+    receiver(md);
 
-    return EXIT_SUCCESS;
+    return (void*) args;
 }
 /* ##################################################################################################################### */
-const int transmitter() {
+const int transmitter(void) {
 
-    // printf("Execution reached transmitter ##########################\n");
     pthread_t threads[THREADS_NUM];
     
     // Dynamically initiallize the thread_ids array;
@@ -132,7 +210,7 @@ const int transmitter() {
     return EXIT_SUCCESS;
 }
 /* ##################################################################################################################### */
-int clientmessage(XEvent *event) {
+const void clientmessage(XEvent *event) {
     
     if (event->xclient.data.l[0] == wmatom[App_Iconify]) {
         printf("Iconify message received!\n");
@@ -143,44 +221,45 @@ int clientmessage(XEvent *event) {
         XDestroyWindow(displ, app);
         XCloseDisplay(displ);
         RUNNING = 0;
-        // printf("client message processed  ******\n");
-        return EXIT_SUCCESS;
     }
-    return EXIT_FAILURE;
 }
 /* ##################################################################################################################### */
-int visibilitychange(XEvent *event) {
+void visibilitychange(XEvent *event) {
 
     printf("visibilitychange event received   ******\n");
     // transmitter();
-    return EXIT_SUCCESS;
+
 }
 /* ##################################################################################################################### */
-int mapnotify(XEvent *event) {
+const void mapnotify(XEvent *event) {
 
     printf("mapnotify event received   ******\n");
+    XGetWindowAttributes(displ, app, &stat_app);
+
+    if (transmitter())
+        perror("mapnotify() --- transmitter()");
+
     // XSync(displ, True);
-    return EXIT_SUCCESS;
 }
 /* ##################################################################################################################### */
-int unmapnotify(XEvent *event) {
+void unmapnotify(XEvent *event) {
 
     printf("unmapnotify event received   ******\n");
     // XSync(displ, True);
-    return EXIT_SUCCESS;
+
 }
 /* ##################################################################################################################### */
-int expose(XEvent *event) {
+void expose(XEvent *event) {
 
     printf("expose event received   ******\n");
-    transmitter();
-    return EXIT_SUCCESS;
+    // transmitter();
+
 }
-int buttonpress(XEvent *event) {
+void buttonpress(XEvent *event) {
 
     printf("buttonpress event received   ******\n");
-    transmitter();
-    return EXIT_SUCCESS;
+    // transmitter();
+
 }
 /* ##################################################################################################################### */
 // General initialization and event handling.
@@ -188,7 +267,8 @@ const int board() {
 
     // printf("board starting initiallization   ******\n");
     // Add some randomness
-    srand(time(NULL));
+    // srand(time(NULL));
+    XInitThreads();
     XEvent event;
     
     displ = XOpenDisplay(NULL);
@@ -253,9 +333,10 @@ int main(int argc, char *argv[]) {
 
     if (locale_init())
         fprintf(stderr, "Warning: Main -locale()\n");
-
-    board();
     
+    if (board())
+        return EXIT_FAILURE;
+
     return EXIT_SUCCESS;
 }
 /* ##################################################################################################################### */
